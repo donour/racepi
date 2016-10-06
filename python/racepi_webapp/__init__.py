bc0d8e54ebd86fe5b3921d73fe9e79b607f6f722
donour <donour@cs.unm.edu>
2016-10-05 23:18:38 -0400

    update python structure

--- python/racepi_webapp/__init__.py
+++ python/racepi_webapp/__init__.py
@@ -3,26 +3,31 @@ from sqlalchemy import create_engine
 
 app = Flask(__name__)
 
-db = create_engine('sqlite:////external/racepi_data/test.db')
+db = create_engine('sqlite:////home/donour/test.db')
 c = db.connect()
+
 
 def get_data(table):
     q = c.execute("select * FROM %s" % table);
     return jsonify(q.cursor.fetchall())
 
+
 @app.route('/')
 def hello_world():
     return app.send_static_file('index.html')
+
 
 @app.route('/sessions')
 def get_sessions():
     return get_data("sessions")
 
+
 @app.route('/gps')
 def get_gps_data():
     return get_data("gps_data")
 
+
 @app.route('/imu')
-def get_imu_data():  
+def get_imu_data():
     return get_data("imu_data")
 
---
+++ python/runwebapp.py
@@ -0,0 +1,2 @@
+from racepi_webapp import app
+app.run(host='0.0.0.0', debug=True)
