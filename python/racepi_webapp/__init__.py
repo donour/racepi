ab825df8466b053b94f9a05a60eb7d27e99ff007
Donour Sizemore <donour@cs.unm.edu>
2016-10-08 12:10:25 -0400

    add simple plotly output rest call

--- python/racepi_webapp/__init__.py
+++ python/racepi_webapp/__init__.py
@@ -1,14 +1,15 @@
 from flask import Flask, jsonify, request
 from sqlalchemy import create_engine
+from plotly import graph_objs as pgo
 
 app = Flask(__name__)
 db = create_engine('sqlite:////home/donour/test.db')
 
 
 def get_sql_data(table, filter):
-    c = db.connect()
-    q = c.execute("select * FROM %s where %s " % (table, filter));
-    return jsonify(result=q.cursor.fetchall())
+    with db.connect() as c:
+        q = c.execute("select * FROM %s where %s " % (table, filter))
+        return jsonify(result=q.cursor.fetchall())
 
 
 def get_data(table):
@@ -35,3 +36,21 @@ def get_gps_data():
 def get_imu_data():
     return get_data("imu_data")
 
+
+@app.route('/plot/timeseries')
+def get_plot_timeseries():
+    session_id = request.args.get("session_id")
+    # TODO fail on bad session_id
+    with db.connect() as c:
+        q = c.execute("select time,speed FROM %s where %s " % ("gps_data", "session_id='%s'" % session_id))
+        x = []
+        y = []
+        data = q.cursor.fetchall()
+        map(lambda p: x.append(p[0]), data)
+        map(lambda p: y.append(p[1]), data)
+        data = [
+            pgo.Scatter(x=x, y=y)
+        ]
+        layout = pgo.Layout(title="Timeseries")
+        fig = pgo.Figure(data=data, layout=layout)
+        return jsonify(result=fig)
