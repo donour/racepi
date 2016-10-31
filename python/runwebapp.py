a0d903af79e34e4876ea018cf25f6a62c9f73cbe
donour <donour@cs.unm.edu>
2016-10-30 23:18:55 -0400

    update copyrights

--- python/runwebapp.py
+++ python/runwebapp.py
@@ -1,2 +1,18 @@
+# Copyright 2016 Donour Sizemore
+#
+# This file is part of RacePi
+#
+# RacePi is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RacePi is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with RacePi.  If not, see <http://www.gnu.org/licenses/>.
 from racepi_webapp import app
 app.run(host='0.0.0.0', debug=True, threaded=True)
