feb3a1cbe98172a3029d184f32a337ff3b397165
donour <donour@cs.unm.edu>
2016-12-07 21:25:43 -0500

    add simple comparison logic

--- python/racepi_bokehapp/racepi_analysis.py
+++ python/racepi_bokehapp/racepi_analysis.py
@@ -50,48 +50,70 @@ class RacePiDBSession:
             ("imu_data", session_id), self.db, index_col='timestamp')
 
 
+class RunView:
+
+    def __init__(self):
+        self.session_selector = None
+        self.stats = PreText(text='', width=500, height=150)
+        self.details = PreText(text='', width=300, height=100)
+        self.speed_source = ColumnDataSource(data=dict(timestamp=[], speed=[], lat=[], lon=[]))
+        self.accel_source = ColumnDataSource(data=dict(timestamp=[], x_accel=[]))
+
+
 class RacePiAnalysis:
 
-    def load_data(self, session_info_primary,  session_info_compares=[]):
+    def load_data(self, session_info, v):
+
 
         # TODO convert all timestamps to deltas
 
         # load primary run data
-        session_id = session_info_primary[0]
+        session_id = session_info[0]
         gps_data = self.db.get_gps_data(session_id)
         imu_data = self.db.get_imu_data(session_id)
         gps_data = gps_data.rolling(min_periods=1, window=8, center=True).mean()
         imu_data = imu_data.rolling(min_periods=1, window=32, center=True).mean()
 
-        self.speed_source.data = ColumnDataSource(gps_data).data
-        self.accel_source.data = ColumnDataSource(imu_data).data
-        self.stats.text = str(gps_data.describe())
-        self.details.text = "date:%s\nduration:%.0f\nVmax:%.0f\nsamples:%d" % session_info[1:5]
+        v.speed_source.data = ColumnDataSource(gps_data).data
+        v.accel_source.data = ColumnDataSource(imu_data).data
+        v.stats.text = str(gps_data.describe())
+        v.details.text = "date:%s\nduration:%.0f\nVmax:%.0f\nsamples:%d" % session_info[1:5]
 
-        # load compare run data
+    def session_change_primary(self, attrname, old, new):
+        self.load_data(self.sessions[new], self.primary_view)
 
-    def session_change(self, attrname, old, new):
-        self.load_data(self.sessions[new])
+    def session_change_compare(self, attrname, old, new):
+        self.load_data(self.sessions[new], self.compare_view)
 
     def __init__(self):
         self.db = RacePiDBSession()
-
         self.sessions = {"%s:%.0f" % (datetime.fromtimestamp(s[1]).isoformat(), s[2]): s for s in self.db.get_sessions()}
-        session_selector = Select(options=self.sessions.keys())
 
-        # set up layout
-        self.stats = PreText(text='', width=500, height=150)
-        self.details = PreText(text='', width=300, height=100)
+        self.primary_view = pv = RunView()
+        self.compare_view = cv = RunView()
 
-        self.speed_source = ColumnDataSource(data=dict(timestamp=[], speed=[], lat=[], lon=[]))
-        self.accel_source = ColumnDataSource(data=dict(timestamp=[], x_accel=[]))
+        pv.session_selector = Select(options=self.sessions.keys())
+        cv.session_selector = Select(options=self.sessions.keys())
+
         s1 = figure(width=800, plot_height=200, title="speed")
-        s1.line('timestamp', 'speed', source=self.speed_source)
+        s1.line('timestamp', 'speed', source=pv.speed_source)
+        s1.line('timestamp', 'speed', source=cv.speed_source)
         s2 = figure(width=800, plot_height=200, title="xaccel", x_range=s1.x_range, y_range=[-1.5, 1.5], tools=[])
-        s2.line('timestamp', 'x_accel', source=self.accel_source)
-        session_selector.on_change('value', self.session_change)
+        s2.line('timestamp', 'x_accel', source=pv.accel_source)
+        s2.line('timestamp', 'x_accel', source=cv.accel_source)
 
-        self.widgets = column(row(self.stats, column(session_selector, self.details)), s1, s2)
+        pv.session_selector.on_change('value', self.session_change_primary)
+        cv.session_selector.on_change('value', self.session_change_compare)
+
+        self.widgets = column(
+            row(
+                column(pv.session_selector, pv.details),
+                column(cv.session_selector, cv.details)
+            ),
+            s1,
+            s2,
+            pv.stats
+        )
 
 
 
