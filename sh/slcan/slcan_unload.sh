4c0711ca55917df68cd85f3339fb1603ccf0ec97
Donour Sizemore <donour@cs.unm.edu>
2017-02-19 10:36:17 -0500

    add slcan load/unload scripts

---
+++ sh/slcan/slcan_unload.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Copyright 2017 Donour Sizemore
+#
+# This file is part of RacePi
+#
+# RacePi is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2.
+#
+# RacePi is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with RacePi.  If not, see <http://www.gnu.org/licenses/>.
+
+# Remove the usb<->can device
+set -eu
+
+echo "Killing slcand driver:" $1 
+exec pkill -f "slcand.*$1"
+
