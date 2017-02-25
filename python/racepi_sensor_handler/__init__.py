fbe8ee2cab7e95a505267bb725b371f58072339a
donour <donour@cs.unm.edu>
2017-02-24 20:51:25 -0500

    replace STN111X can handler with socketcan

--- python/racepi_sensor_handler/__init__.py
+++ python/racepi_sensor_handler/__init__.py
@@ -1,3 +1,21 @@
+# Copyright 2017 Donour Sizemore
+#
+# This file is part of RacePi
+#
+# RacePi is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RacePi is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with RacePi.  If not, see <http://www.gnu.org/licenses/>.
+
 from .stn11xx_can_handler import STN11XXCanSensorHandler
 from .gps_handler import GpsSensorHandler, GPS_REQUIRED_FIELDS
 from .pi_sense_hat_imu import RpiImuSensorHandler
+from .socketcan_handler import SocketCanSensorHandler
