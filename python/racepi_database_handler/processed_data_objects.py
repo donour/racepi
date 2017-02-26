1a1e58c52096b28cfb02117ca36a1b5f64bbfbc6
donour <donour@cs.unm.edu>
2017-02-26 13:50:29 -0500

    Add custom ORM object for decoding can frames

--- python/racepi_database_handler/processed_data_objects.py
+++ python/racepi_database_handler/processed_data_objects.py
@@ -13,5 +13,15 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with RacePi.  If not, see <http://www.gnu.org/licenses/>.
-from .objects import *
-from .processed_data_objects import *
+
+from .objects import Base, CANData
+from racepi_can_decoder import CanFrame
+
+
+class ProcessedCANSample(CANData):
+
+    def value(self, can_frame_value_extractor):
+        # convert fields to can frame and extract value
+        # arbitration id is not used in conversion
+        f = CanFrame("000", self.msg)
+        return can_frame_value_extractor.convert_frame(f)
