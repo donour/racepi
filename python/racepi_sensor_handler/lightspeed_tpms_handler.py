9c0697ae7b4a8975f2493bf59035d76ccf41eec9
donour <donour@cs.unm.edu>
2017-05-14 18:37:10 -0400

    initial bluetooth code for lightspeed TPMS

---
+++ python/racepi_sensor_handler/lightspeed_tpms_handler.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# Copyright 2017 Donour Sizemore
+#
+# This file is part of RacePi
+#
+# RacePi is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2.
+#
+# RacePi is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with RacePi.  If not, see <http://www.gnu.org/licenses/>.
+"""
+This sensor module supports the bluetooth TPMS sensors from Lightspeed/Macsboost.
+"""
+
+# TODO: untested
+
+import bluetooth as bt
+
+from .sensor_handler import SensorHandler
+
+DEFAULT_TPMS_NAME = 'TPMS'
+TPMS_BT_PORT = 6
+
+
+class LightSpeedTPMSSensorHandler(SensorHandler):
+
+    def __init__(self, tpms_name=DEFAULT_TPMS_NAME, bt_port=TPMS_BT_PORT):
+        self.tpms_name = tpms_name
+        self.port = bt_port
+        self.sock = None
+
+    def __connect(self):
+        """
+        Search for TPMS device and connect to the RFCOMM service on
+        port 6.
+        :return: 
+        """
+        dev_addr = None
+        while not dev_addr:
+            nearby_devices = bt.discover_devices(lookup_names=True)
+            for addr, name in nearby_devices:
+                if self.tpms_name in name:
+                    print("tpms: %s - %s" % (addr, name))
+                    dev_addr = addr
+
+        # Create the client socket
+        self.sock = bt.BluetoothSocket(bt.RFCOMM )
+        self.sock.connect((dev_addr, self.port))
+
+    def __record_tpms(self):
+        if not self.pipe_out:
+            raise ValueError("Illegal argument, no queue specified")
+
+        print("Starting LightSpeed TPMS reader")
+        while not self.doneEvent.is_set():
+            if not self.sock:
+                self.__connect()
+            d = self.sock.recv(40)
+            print(d)
+            # TODO, parse and post the data to the rx queue
+
+        if self.sock:
+            self.sock.close()
+
+
